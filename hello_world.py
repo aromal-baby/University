33d471a7d4f163f6df1a2b364f098e379986faa6
aromal-baby <aromalbaby365@gmail.com>
2024-10-22 14:27:28 +0200

    Add files to repo

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+
+"""
+A project outlineing the basic input-output methods in python
+
+"""
+
+#print a welcome message
+print('hello world!')
+
+#Ask the user for their name
+user_name = input("What is your name?")
+
+#print the user name with a welcome message
+print("Welcome" + user_name + " to python programming")
+
+
